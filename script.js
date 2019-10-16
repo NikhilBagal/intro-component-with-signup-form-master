06bbbb4cf1aa01c9197e496f2b3884d2eea89af6
nikhil bagal <nbagal02@gmail.com>
2019-10-16 23:19:16 +0530

    js code changed for validation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function init(){
+/*function init(){
     var element = document.querySelectorAll('input');
     for(let i of element){
         if(i.value !== ''){
@@ -11,7 +11,7 @@ function init(){
             }*/
            /* let reg= /\w/gi;
             console.log(reg.test(i.value));*/
-            if(i.id === 'name-input'){
+        /*    if(i.id === 'name-input'){
                 let reg= /[a-z]/gi;
                 if(reg.test(i.value)){
                     var ele = document.querySelector("#"+i.id+'-error');
@@ -45,4 +45,89 @@ function init(){
             ele.classList.add('active');
         }
     }
+}*/
+/*function init(){
+    var element = document.querySelectorAll('input');
+    for(let i of element){
+     //   i.addEventListener("onchange", function(){
+            if(i.value !== ''){
+                switch(i.id){
+                    case 'name-input':
+                        var reg= /[a-z]/gi;
+                        if(reg.test(i.value)){
+                            var ele = document.querySelector("#"+i.id+'-error');
+                            ele.classList.remove('active');
+                        }else {
+                            var ele = document.querySelector("#"+i.id+'-error');
+                            ele.classList.add('active');
+                        }
+                        break;
+                    case 'Last-name-input':
+                        var reg= /[a-z]/gi;
+                        if(reg.test(i.value)){
+                            var ele = document.querySelector("#"+i.id+'-error');
+                            ele.classList.remove('active');
+                        }else {
+                            var ele = document.querySelector("#"+i.id+'-error');
+                            ele.classList.add('active');
+                        }
+                        break;
+                    case 'mail-input':
+                        var reg = /\w+@\w+\.\w+/gi;
+                        if(reg.test(i.value)){
+                            var ele = document.querySelector("#"+i.id+'-error');
+                            ele.classList.remove('active');
+                        }else {
+                            var ele = document.querySelector("#"+i.id+'-error');
+                            ele.classList.add('active');
+                        }
+                        break;
+                }
+            }else {
+                var ele = document.querySelector("#"+i.id+'-error');
+                ele.classList.add('active');
+              }
+     //   })
+    }
+}*/
+function init(){
+    var i = event.target;
+    //console.log(element);
+    if(i.value !== ''){
+        switch(i.id){
+            case 'name-input':
+                var reg= /[a-z]/gi;
+                if(reg.test(i.value)){
+                    var ele = document.querySelector("#"+i.id+'-error');
+                    ele.classList.remove('active');
+                }else {
+                    var ele = document.querySelector("#"+i.id+'-error');
+                    ele.classList.add('active');
+                }
+                break;
+            case 'Last-name-input':
+                var reg= /[a-z]/gi;
+                if(reg.test(i.value)){
+                    var ele = document.querySelector("#"+i.id+'-error');
+                    ele.classList.remove('active');
+                }else {
+                    var ele = document.querySelector("#"+i.id+'-error');
+                    ele.classList.add('active');
+                }
+                break;
+            case 'mail-input':
+                var reg = /\w+@\w+\.\w+/gi;
+                if(reg.test(i.value)){
+                    var ele = document.querySelector("#"+i.id+'-error');
+                    ele.classList.remove('active');
+                }else {
+                    var ele = document.querySelector("#"+i.id+'-error');
+                    ele.classList.add('active');
+                }
+                break;
+        }
+    }else {
+        var ele = document.querySelector("#"+i.id+'-error');
+        ele.classList.add('active');
+      }
 }
