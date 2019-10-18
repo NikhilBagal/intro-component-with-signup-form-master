dab1c504751b525c6c104ebf25bfcd73937e771c
nikhil bagal <nbagal02@gmail.com>
2019-10-18 14:20:00 +0530

    js code changed to more efficient

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function init(){
+/*function init(){
     var i = event.target;
     if(i.value !== ''){
         var element = document.querySelector("#"+i.id+'-error');
@@ -41,4 +41,29 @@ function init(){
         var element = document.querySelector("#"+i.id+'-format-error');
         element.classList.remove('active');
       }
+}
+*/
+let obj = {
+    'name-input' : /^[a-z]+$/gi,
+    'Last-name-input' : /^[a-z]+$/gi,
+    'mail-input': /\w+@\w+\.\w+/gi,
+}
+function init(){
+    var element = event.target;
+    if(element.value !== ''){
+        var errorSelector = document.querySelector("#"+element.id+'-error');
+        errorSelector.classList.remove('active');
+        if(obj[element.id].test(element.value) == true){
+            var formatErrorSelector = document.querySelector('#'+element.id+'-format-error');
+            formatErrorSelector.classList.remove('active');
+        }else if(obj[element.id].test(element.value) == false) {
+            var formatErrorSelector = document.querySelector('#'+element.id+'-format-error');
+            formatErrorSelector.classList.add('active');
+        }
+    }else {
+        var errorSelector = document.querySelector("#"+element.id+'-error');
+        errorSelector.classList.add('active');
+        var formatErrorSelector = document.querySelector('#'+element.id+'-format-error');
+        formatErrorSelector.classList.remove('active');
+    }
 }
